479aab5da7717aebdd1ac68ee4d7fc25a5e222b1
ovieBd <oviecse48@gmail.com>
2023-12-03 08:57:35 +0600

    updated code

--- AsynchronousTasks_Async-Let.playground/Contents.swift
+++ AsynchronousTasks_Async-Let.playground/Contents.swift
@@ -10,35 +10,40 @@ private func delayAfter(delay: Int) async throws -> Int {
 }
 
 private func SynchronousTask() async throws{
-    let startTime = DispatchTime.now()
-    
     let firstDelay = try await delayAfter(delay: 4)
     print("First Delay\(firstDelay)")
     let secondDelay = try await delayAfter(delay: 2)
     print("Second Delay\(secondDelay)")
-    
-    let endTime = DispatchTime.now()
-    let elapsedTime = Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000
-    print("ElapsedTime \(elapsedTime)")
 }
 
 
 private func ASynchronousTask() async throws{
-    let startTime = DispatchTime.now()
    
     async let firstDelay = delayAfter(delay: 4)
     async let secondDelay = delayAfter(delay: 2)
-  
-    print("Second Delay\(try await secondDelay)")
     print("First Delay\(try await firstDelay)")
+    print("Second Delay\(try await secondDelay)")
+   
+}
+
+//private func loopAsync() async throws {
+//    let ids = [1,2,3,4,5]
+//    for id in ids {
+//        let delay = try await delayAfter(delay: id)
+//        print(delay)
+//    }
+//    
+//}
+
+
+Task {
+    let startTime = DispatchTime.now()
+  
+   // try await loopAsync()
+   // try await SynchronousTask()
+    try await ASynchronousTask()
     
     let endTime = DispatchTime.now()
     let elapsedTime = Double(endTime.uptimeNanoseconds - startTime.uptimeNanoseconds) / 1_000_000_000
     print("ElapsedTime \(elapsedTime)")
-}
-
-
-Task {
-  //  try await SynchronousTask()
-    try await ASynchronousTask()
 }
